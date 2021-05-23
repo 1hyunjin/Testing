2865281d7393efca1bf1a4b1d9f1dddc81ca869c
1hyunjin <a92046503@gmai.com>
2021-05-24 00:54:41 +0900

    Initial commit

---
+++ src/test/java/MinTest.java
@@ -0,0 +1,81 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MinTest {
+
+    ArrayList<String> list = new ArrayList<String>();
+    @Test
+    void testNullList() {
+        list = null;
+        try {
+            Min.min(list);
+            System.out.println("null");
+        }catch( NullPointerException e) {
+            return;
+        }
+    }
+    @Test
+    void testNullElementwithMultipleElements() {
+        list.add(null);
+        list.add("aa");
+        try {
+            Min.min(list);
+        }catch( NullPointerException e) {
+            return;
+        }
+
+    }
+    @Test
+    void testNulllementwithMultipleElements2() {
+        list.add("aa");
+        list.add(null);
+        list.add("bb");
+    }
+
+    @Test
+    void testNullSingleElement() {
+        list.add(null);
+        try {
+            Min.min(list);
+        }catch( NullPointerException e) {
+            return;
+        }
+    }
+
+    @Test
+    void testEmptyElement() {
+        try {
+            Min.min(list);
+        }catch(IllegalArgumentException i) {
+            return;
+        }
+    }
+
+    @Test
+    void testSingleElement() {
+
+        list.add("aa");
+        Object obj = Min.min(list);
+        assertNotNull(Min.min(list));
+        assertTrue(obj.equals("aa"), "false");
+    }
+    @Test
+    public void testTwoElements() {
+        list.add("bb");
+        list.add("aa");
+        Object obj = Min.min(list);
+        assertEquals(true, obj.equals("aa"));
+    }
+
+    @Test
+    public void testTwoElements2() {
+        list.add("bb");
+        list.add("aa");
+        Object obj = Min.min(list);
+        assertTrue(obj.equals("aa"), "false");
+    }
+
+}
